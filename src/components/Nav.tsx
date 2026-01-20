99788e2ec40be1fcd006fd4e87e90febd16b7dce
anhcu6868 <tuananhcuhuynh@gmail.com>
2026-01-20 14:29:58 +0700

    Create mobile nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,9 +1,11 @@
 import NikeLogo from '../assets/nike-logo.svg?react'
 import { RxHamburgerMenu } from 'react-icons/rx'
 
+const ROUTES = ['Home', 'About', 'Services', 'Pricing', 'Contact']
+
 const Nav = () => {
   return (
-    <nav className="flex items-center justify-between">
+    <nav className="flex flex-wrap items-center justify-between">
       {/* Logo */}
       <a href="#">
         <NikeLogo className="h-20 w-20" />
@@ -12,6 +14,19 @@ const Nav = () => {
       <button className="cursor-pointer rounded-lg p-2 hover:bg-gray-100 focus:ring-2 focus:ring-gray-200">
         <RxHamburgerMenu size={25} />
       </button>
+      {/* Menu list */}
+      <div className="w-full">
+        <ul className="rounded-lg border border-gray-200 bg-gray-50 p-4 text-lg">
+          {ROUTES.map((route, i) => (
+            <li
+              className={`cursor-pointer rounded px-3 py-2 ${i === 0 ? 'bg-blue-500 text-white' : 'hover:bg-gray-100'}`}
+              key={route}
+            >
+              {route}
+            </li>
+          ))}
+        </ul>
+      </div>
     </nav>
   )
 }
