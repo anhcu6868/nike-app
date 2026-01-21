9dfc1752f312ece01f6275773bd8328e9df023a6
anhcu6868 <tuananhcuhuynh@gmail.com>
2026-01-21 17:05:03 +0700

    Making gradient and fonts

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
+import '@fontsource-variable/nunito-sans'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
 
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App/>
+    <App />
   </StrictMode>,
 )
