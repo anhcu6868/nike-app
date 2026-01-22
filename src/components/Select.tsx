7e64ee48ac58978cb2298906d15a6e3be5f64e22
anhcu6868 <tuananhcuhuynh@gmail.com>
2026-01-22 20:47:52 +0700

    Making select

---
+++ src/components/Select.tsx
@@ -0,0 +1,31 @@
+import { IoIosArrowDown } from 'react-icons/io'
+import type { FC } from 'react'
+
+type SelectProps = {
+  title: string
+  options: Array<number>
+}
+const Select: FC<SelectProps> = ({ title, options }) => {
+  return (
+    <div className="relative">
+      <select
+        defaultValue={''}
+        className="w-24 appearance-none border border-gray-300 bg-white p-4"
+      >
+        <option value="" disabled hidden>
+          {title}
+        </option>
+        {options.map((option) => (
+          <option value={option} key={option}>
+            {option}
+          </option>
+        ))}
+      </select>
+      <div className="flex-center absolute inset-y-0 right-0 pr-3">
+        <IoIosArrowDown />
+      </div>
+    </div>
+  )
+}
+
+export default Select
--- src/components/ShoeDetail.tsx
+++ src/components/ShoeDetail.tsx
@@ -1,4 +1,6 @@
 import nike1 from '../assets/n1-min.png'
+import Select from './Select'
+import { SIZES, QTY } from '../constants'
 
 const ShoeDetail = () => {
   return (
@@ -18,7 +20,12 @@ const ShoeDetail = () => {
             "The Nike Air Max 270 is a lifestyle shoe that's sure to turn heads with its vibrant color gradient"
           }
         </div>
-        <div className="text-3xl font-extrabold md:text-6xl">100$</div>
+        {/* Dollars, QTY & SIZE Select */}
+        <div className="flex space-x-6">
+          <div className="text-3xl font-extrabold md:text-6xl">100$</div>
+          <Select title={'QTY'} options={QTY} />
+          <Select title={'SIZE'} options={SIZES} />
+        </div>
         {/* Shoe buttons and links */}
         <div className="space-x-10">
           <button className="h-14 w-44 bg-black text-white hover:bg-gray-900 active:bg-gray-700">
