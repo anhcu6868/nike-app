2f7e49716a9dec2bfbfd83e9c41d2db39a0276af
anhcu6868 <tuananhcuhuynh@gmail.com>
2026-01-24 17:59:54 +0700

    Building Card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import Nav from './components/Nav'
-import ShoeDetail from './components/ShoeDetail'
+// import Nav from './components/Nav'
+// import ShoeDetail from './components/ShoeDetail'
+
+import Card from './components/Card'
+import { SHOE_LIST } from './constants'
 
 const App = () => {
   return (
     <div className="animate-fadeIn p-10 xl:px-24">
-      <Nav />
-      <ShoeDetail />
+      {/* <Nav />
+      <ShoeDetail /> */}
+      <Card item={SHOE_LIST[0]} />
     </div>
   )
 }
