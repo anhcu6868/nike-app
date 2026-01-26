d9ceac718a413e1a017b474f9645918a0bd06e55
anhcu6868 <tuananhcuhuynh@gmail.com>
2026-01-26 19:09:52 +0700

    Setup dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
-import { useState } from 'react'
+import { BiMoon } from 'react-icons/bi'
+import { BiSun } from 'react-icons/bi'
+import { useEffect, useState } from 'react'
 import Nav from './components/Nav'
 import NewArrivalsSection from './components/NewArrivalsSection'
 import ShoeDetail from './components/ShoeDetail'
@@ -15,6 +17,21 @@ const FAKE_CART_ITEMS = SHOE_LIST.map((shoe) => {
 })
 const App = () => {
   const [isSidebarOpen, setSidebarOpen] = useState(false)
+  // Get the value from local storage on component mount
+  useEffect(() => {
+    if (localStorage.getItem('isDarkMode') === 'true') {
+      window.document.documentElement.classList.add('dark')
+    }
+  }, [])
+  const toggleDarkMode = () => {
+    window.document.documentElement.classList.toggle('dark')
+    // Save to local storage
+    if (window.document.documentElement.classList.contains('dark')) {
+      localStorage.setItem('isDarkMode', 'true')
+    } else {
+      localStorage.setItem('isDarkMode', 'false')
+    }
+  }
   return (
     <div className="animate-fadeIn p-10 xl:px-24">
       <Nav onClickShoppingBtn={() => setSidebarOpen(true)} />
@@ -23,6 +40,16 @@ const App = () => {
       <Sidebar onClose={() => setSidebarOpen(false)} isOpen={isSidebarOpen}>
         <Cart cartItems={FAKE_CART_ITEMS} />
       </Sidebar>
+      {/* Darkmode */}
+      <div className="fixed right-4 bottom-4">
+        <button
+          onClick={toggleDarkMode}
+          className="bg-night-50 dark:text-night-50 cursor-pointer rounded-full px-4 py-2 text-white shadow-lg dark:bg-white"
+        >
+          <BiSun className="hidden dark:block" />
+          <BiMoon className="dark:hidden" />
+        </button>
+      </div>
     </div>
   )
 }
